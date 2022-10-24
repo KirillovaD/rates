7220706a2f57d792e3d47b16e1bef4bb6e7d87c2
KirillovaD <sdaria@inbox.ru>
2022-10-24 11:14:15 +0300

    add styles

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
 import "./App.css";
 import Rate from "./Rate";
-import colorRate1 from "./colorRate1.scss";
-import colorRate2 from "./colorRate2.scss";
-import colorRate3 from "./colorRate3.scss";
-import colorRate4 from "./colorRate4.scss";
+import color1 from "./colorRate1.module.scss";
+import color2 from "./colorRate2.module.scss";
+import color3 from "./colorRate3.module.scss";
+import color4 from "./colorRate4.module.scss";
 
 const rates = [
   {
     name: 300,
     price: 300,
     trafficValue: 10,
-    color: colorRate1,
+    color: color1,
   },
   {
     name: 450,
     price: 450,
     trafficValue: 50,
-    color: colorRate2,
+    color: color2,
   },
   {
     name: 550,
     price: 550,
     trafficValue: 100,
-    color: colorRate3,
+    color: color3,
     isSelected: true,
   },
   {
     name: 1000,
     price: 1000,
     trafficValue: 200,
-    color: colorRate4,
+    color: color4,
   },
 ];
 console.log(rates.color);
--- src/Rate.jsx
+++ src/Rate.jsx
@@ -7,10 +7,20 @@ export default class Rate extends React.Component {
 
     const classRate = isSelected ? "rate__card-selected" : "rate__card";
     return (
-      <div className={`${classRate}`}>
-        <div className={color.card__header}>{`Безлимитный ${name}`}</div>
-        <div className={color.card__price}>{`руб ${price} /мес `}</div>
-        <div className="card__trafficValue">{`до ${trafficValue} Мбит/сек`}</div>
+      <div className={`rate__card ${classRate}`}>
+        <div className={`card__header ${color.card__header_color} `}>
+          <p>{`Безлимитный ${name}`}</p>
+        </div>
+        <div className={"card__price " + `${color.card__price_color}`}>
+          <p>
+            <span className="small_up">руб</span>
+            {`${price}`}
+            <span className="small_down">/мес</span>
+          </p>
+        </div>
+        <div className="card__trafficValue">
+          <p>{`до ${trafficValue} Мбит/сек`}</p>
+        </div>
         <div className="card__text">
           Объем включенного <br /> трафика не ограничен
         </div>
